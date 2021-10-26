9a63ef09478122fc9271be33c6c93d1b1bdd903c
houzairkoussa <houzairmk@icloud.com>
2021-10-26 13:44:55 +0400

    user can view a single article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+  before_action :get_article, only: [:show]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  private
+
+  def get_article
+    @article = Article.find(params[:id])
+  end
+end
